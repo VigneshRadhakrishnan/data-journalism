b9c4e7483cd2e3dc55c8aa3616e08ec45787018d
VigneshRadhakrishnan <vignesh.journalist@gmail.com>
2024-11-14 10:25:47 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Basic form submission alert
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you for reaching out! We'll get back to you soon.");
+});
